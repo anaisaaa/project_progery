af25053b74caea6684804814ba69b18cecf7f34d
valeriyagrin <1valeriyagrin@gmail.com>
2025-03-24 23:13:19 +0300

    ConvexShape.hpp

--- graphics/ConvexShape.hpp
+++ graphics/ConvexShape.hpp
@@ -3,22 +3,20 @@
 // SFML - Simple and Fast Multimedia Library
 // Copyright (C) 2007-2018 Laurent Gomila (laurent@sfml-dev.org)
 //
-// This software is provided 'as-is', without any express or implied warranty.
-// In no event will the authors be held liable for any damages arising from the use of this software.
+// Данное программное обеспечение предоставляется "как есть", без каких-либо гарантий.
+// Авторы не несут ответственности за любые убытки, вызванные использованием этого ПО.
 //
-// Permission is granted to anyone to use this software for any purpose,
-// including commercial applications, and to alter it and redistribute it freely,
-// subject to the following restrictions:
+// Разрешается использовать это ПО для любых целей, включая коммерческие приложения,
+// а также модифицировать и свободно распространять при соблюдении следующих условий:
 //
-// 1. The origin of this software must not be misrepresented;
-//    you must not claim that you wrote the original software.
-//    If you use this software in a product, an acknowledgment
-//    in the product documentation would be appreciated but is not required.
+// 1. Нельзя выдавать себя за автора оригинального ПО;
+//    запрещено утверждать, что вы написали оригинальное ПО.
+//    При использовании в продукте, указание авторства приветствуется, но не обязательно.
 //
-// 2. Altered source versions must be plainly marked as such,
-//    and must not be misrepresented as being the original software.
+// 2. Измененные версии должны быть явно обозначены как таковые
+//    и не должны выдаваться за оригинальное ПО.
 //
-// 3. This notice may not be removed or altered from any source distribution.
+// 3. Данное уведомление не может быть удалено или изменено.
 //
 ////////////////////////////////////////////////////////////
 
@@ -26,17 +24,16 @@
 #define SFML_CONVEXSHAPE_HPP
 
 ////////////////////////////////////////////////////////////
-// Headers
+// Заголовочные файлы
 ////////////////////////////////////////////////////////////
 #include <SFML/Graphics/Export.hpp>
 #include <SFML/Graphics/Shape.hpp>
 #include <vector>
 
-
 namespace sf
 {
 ////////////////////////////////////////////////////////////
-/// \brief Specialized shape representing a convex polygon
+/// \brief Специализированная фигура, представляющая выпуклый многоугольник
 ///
 ////////////////////////////////////////////////////////////
 class SFML_GRAPHICS_API ConvexShape : public Shape
@@ -44,19 +41,19 @@ class SFML_GRAPHICS_API ConvexShape : public Shape
 public:
 
     ////////////////////////////////////////////////////////////
-    /// \brief Default constructor
+    /// \brief Конструктор по умолчанию
     ///
-    /// \param pointCount Number of points of the polygon
+    /// \param pointCount Количество вершин многоугольника
     ///
     ////////////////////////////////////////////////////////////
     explicit ConvexShape(std::size_t pointCount = 0);
 
     ////////////////////////////////////////////////////////////
-    /// \brief Set the number of points of the polygon
+    /// \brief Установка количества вершин многоугольника
     ///
-    /// \a count must be greater than 2 to define a valid shape.
+    /// \a count должен быть больше 2, чтобы задать валидную фигуру.
     ///
-    /// \param count New number of points of the polygon
+    /// \param count Новое количество вершин многоугольника
     ///
     /// \see getPointCount
     ///
@@ -64,9 +61,9 @@ public:
     void setPointCount(std::size_t count);
 
     ////////////////////////////////////////////////////////////
-    /// \brief Get the number of points of the polygon
+    /// \brief Получение количества вершин многоугольника
     ///
-    /// \return Number of points of the polygon
+    /// \return Количество вершин многоугольника
     ///
     /// \see setPointCount
     ///
@@ -74,16 +71,16 @@ public:
     virtual std::size_t getPointCount() const;
 
     ////////////////////////////////////////////////////////////
-    /// \brief Set the position of a point
+    /// \brief Установка позиции вершины
     ///
-    /// Don't forget that the polygon must remain convex, and
-    /// the points need to stay ordered!
-    /// setPointCount must be called first in order to set the total
-    /// number of points. The result is undefined if \a index is out
-    /// of the valid range.
+    /// Важно помнить, что многоугольник должен оставаться выпуклым,
+    /// а вершины должны быть заданы в правильном порядке!
+    /// Сначала необходимо вызвать setPointCount, чтобы установить общее
+    /// количество вершин. Результат не определен, если \a index выходит
+    /// за допустимые границы.
     ///
-    /// \param index Index of the point to change, in range [0 .. getPointCount() - 1]
-    /// \param point New position of the point
+    /// \param index Индекс изменяемой вершины (в диапазоне [0 .. getPointCount() - 1])
+    /// \param point Новая позиция вершины
     ///
     /// \see getPoint
     ///
@@ -91,16 +88,16 @@ public:
     void setPoint(std::size_t index, const Vector2f& point);
 
     ////////////////////////////////////////////////////////////
-    /// \brief Get the position of a point
+    /// \brief Получение позиции вершины
     ///
-    /// The returned point is in local coordinates, that is,
-    /// the shape's transforms (position, rotation, scale) are
-    /// not taken into account.
-    /// The result is undefined if \a index is out of the valid range.
+    /// Возвращаемая точка находится в локальных координатах,
+    /// то есть трансформации фигуры (позиция, вращение, масштаб)
+    /// не учитываются.
+    /// Результат не определен, если \a index выходит за допустимые границы.
     ///
-    /// \param index Index of the point to get, in range [0 .. getPointCount() - 1]
+    /// \param index Индекс получаемой вершины (в диапазоне [0 .. getPointCount() - 1])
     ///
-    /// \return Position of the index-th point of the polygon
+    /// \return Позиция вершины с указанным индексом
     ///
     /// \see setPoint
     ///
@@ -110,31 +107,29 @@ public:
 private:
 
     ////////////////////////////////////////////////////////////
-    // Member data
+    // Данные-члены
     ////////////////////////////////////////////////////////////
-    std::vector<Vector2f> m_points; ///< Points composing the convex polygon
+    std::vector<Vector2f> m_points; ///< Вершины, составляющие выпуклый многоугольник
 };
 
 } // namespace sf
 
-
 #endif // SFML_CONVEXSHAPE_HPP
-
 
 ////////////////////////////////////////////////////////////
 /// \class sf::ConvexShape
 /// \ingroup graphics
 ///
-/// This class inherits all the functions of sf::Transformable
-/// (position, rotation, scale, bounds, ...) as well as the
-/// functions of sf::Shape (outline, color, texture, ...).
+/// Этот класс наследует все функции sf::Transformable
+/// (позиция, вращение, масштаб, границы и т.д.), а также
+/// функции sf::Shape (контур, цвет, текстура и т.д.).
 ///
-/// It is important to keep in mind that a convex shape must
-/// always be... convex, otherwise it may not be drawn correctly.
-/// Moreover, the points must be defined in order; using a random
-/// order would result in an incorrect shape.
+/// Важно помнить, что выпуклая фигура должна всегда оставаться
+/// выпуклой, иначе она может отображаться некорректно.
+/// Кроме того, вершины должны быть заданы в правильном порядке;
+/// случайный порядок приведет к некорректной форме.
 ///
-/// Usage example:
+/// Пример использования:
 /// \code
 /// sf::ConvexShape polygon;
 /// polygon.setPointCount(3);
